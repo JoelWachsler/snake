02f96e12c98b9e8aa5b0e4620cefcef8978e9280
Joel Wachsler <joel@wachsler.se>
2017-03-12 16:40:13 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-	mysql_connect('localhost', 'joelwr', 'PW')or die('Failed on server');
-	mysql_select_db('joelwr') or die('No db');
+mysql_connect('localhost', 'joelwr', 'DB_PASSWORD_HERE')or die('Failed on server');
+mysql_select_db('joelwr') or die('No db');
 ?>
