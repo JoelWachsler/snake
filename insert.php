2487601aea4c3a8aee333a5413d236cc197617ad
Joel Wachsler <joel@wachsler.se>
2017-10-14 17:38:28 +0200

    Sql-injection fixed

--- insert.php
+++ insert.php
@@ -6,19 +6,19 @@ if (isset($_POST[name]) && isset($_POST[score]) && $_POST[score] != "" && $_POST
 
 	$con=mysqli_connect("","dbname","password","table");
 	// Check connection
-	if (mysqli_connect_errno())
-	  {
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	  }
-
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+	$name = mysqli_real_escape_string($con,$_POST['name']);
+	$score = mysqli_real_escape_string($con,$_POST['score']);
 	$sql="INSERT INTO snake (name, score)
 	VALUES
-	('$_POST[name]','$_POST[score]')";
+	('$name','$score')";
 
-	if (!mysqli_query($con,$sql))
-	  {
-	  die('Error: ' . mysqli_error($con));
-	  }
+	if (!mysqli_query($con,$sql)) {
+		die('Error: ' . mysqli_error($con));
+	}
 	echo 'true';
 
 	mysqli_close($con);
